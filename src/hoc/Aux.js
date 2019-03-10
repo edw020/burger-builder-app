83d0a67e4b039adde88abe80de0b7eab6639b3cc
Victor Edward Calle <victor@bybsoft.com>
2019-03-09 19:44:16 -0600

    Including base structure for the project by defining the layout, navigation and main page components.

---
+++ src/hoc/Aux.js
@@ -0,0 +1,6 @@
+/**
+ * Auxiliary container created as a hoc for wrapping multiple same level components (Works pretty much like React.Fragment for now)
+ */
+const aux = props => props.children;
+
+export default aux;
