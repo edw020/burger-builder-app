5cf8a3a78d00be0daf235f1bbcb202926639dec0
Victor Edward Calle <victor@bybsoft.com>
2019-03-22 21:31:55 -0600

    Adding routing logic for site, and completing the base logic for checkout form.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,30 +75,15 @@ class BurgerBuilder extends Component{
     };
 
     purchaseContinueHandler = () => {
-
-        this.setState({loading: true});
-
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Edward Calle',
-                address: {
-                    street: '1st Avenue',
-                    zipCode: '11111',
-                    country: 'Costa Rica'
-                },
-                email: 'myemail@example.com'
-            },
-            deliveryMethod: 'fastest'
-        };
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: true, purchasing: false});
-            });
+        const queryParams = [];
+        for(let i in this.state.ingredients)
+            queryParams.push(encodeURIComponent(i)+'='+encodeURIComponent(this.state.ingredients[i]));
+        queryParams.push('price='+this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?'+queryString
+        });
     };
 
     render(){
